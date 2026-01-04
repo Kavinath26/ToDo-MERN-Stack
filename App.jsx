962f7e779fa5f9ee7f92706ae601548cf133f3a9
Kavinath K <gkkavinath26042005@gmail.com>
2026-01-04 16:28:22 +0530

    frontend

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Todo from './Todo'
+
+
+function App() {
+
+  return (
+    <Todo />
+  )
+}
+
+export default App
